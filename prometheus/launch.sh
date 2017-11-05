6ca869f12613b82f88b0150d023c834a8286fd4f
David Benque <dbenque@gmail.com>
2017-11-06 00:15:51 +0100

    add node exporter for local scrape

--- prometheus/launch.sh
+++ prometheus/launch.sh
@@ -4,6 +4,8 @@ BASEDIR=$(dirname "$0")
 
 docker stop prom-local
 docker stop prom-fed
+docker stop prom-nodeexporter
 sleep 2
 docker run -d --name=prom-local --net=host -v $PWD/local.prometheus.yml:/etc/prometheus/prometheus.yml --rm prom/prometheus
 docker run -d --name=prom-fed --net=host -v $PWD/federation.prometheus.yml:/etc/prometheus/prometheus.yml --rm prom/prometheus -web.listen-address=:9091 -config.file=/etc/prometheus/prometheus.yml -storage.local.path=/prometheus -web.console.libraries=/usr/share/prometheus/console_libraries -web.console.templates=/usr/share/prometheus/consoles
+docker run -d --name=prom-nodeexporter --net="host" --pid="host" --rm prom/node-exporter
