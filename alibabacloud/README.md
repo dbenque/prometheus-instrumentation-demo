5e1932721f5a8204659ee00857f4d661f116ce8f
David Benque <dbenque@gmail.com>
2017-11-12 01:52:10 +0100

    alibaba cloud prometheus operator, monitoring namespace

---
+++ alibabacloud/README.md
@@ -0,0 +1,111 @@
+# install kubernetes
+
+Doc : https://www.alibabacloud.com/help/doc-detail/53752.htm
+
+https://ros.console.aliyun.com/
+
+--> go to sample template and select the kubernetes sample (HA or TEST)
+
+--> select a region. Note: You can only use ROS templates to deploy Kubernetes clusters in US West 1, China North 2, Asia Pacific SE 1, China East 1, China East 2, China South 1 and Hong Kong regions.
+
+--> Select number of instances and types (doc here https://www.alibabacloud.com/help/faq-detail/43207.htm):  for example 2 nodes ecs.n2.medium
+
+# Export kubeconfig using nc
+
+On the kubernetes master node ( 47.91.156.77 in this example )
+
+install netcat
+```
+yum install nc
+```
+
+then netcat the configuration
+```
+cat /etc/kubernetes/kube.conf | nc -l -p 40000
+```
+
+On the remote machine
+
+```
+IP=47.91.156.77; nc $IP 40000 | sed -e "s/1.1.1.1/$IP/g" | sed -e "s/kubernetes-admin@kubernetes/alibaba/g" -e "s/kubernetes-admin/alibaba/g" -e "s/kubernetes/alibaba/g" > ~/.kube/alibaba.conf
+
+```
+
+If you plan to work in a multi-context environment then check the project **https://github.com/dbenque/k8s-ps1** use **kcontext.sh**
+```
+export KUBECONFIG=$KUBECONFIG:~/.kube/alibaba.conf
+kubectl config use-context alibaba
+```
+
+The insecure option must be use to skip certifiacte validation. If you are using multi-context you should remove the **--kubeconfig** option
+
+```
+alias kk='kubectl --kubeconfig=$HOME/.kube/alibaba.conf --insecure-skip-tls-verify'
+```
+
+Try to check
+```
+kk get pods --all-namespaces
+```
+
+# install the application
+
+```
+kk run resto --image=dbenque/prom-demo-resto:v2 --expose --port=8080
+```
+
+Note: I am not using ngnix ingress because the I am facing problem with content-type overwrite that prevent correct loading of css and js resources that are sent as "text/html"
+
+Move the service **resto** as node port
+```
+PORTRESTO=$(kk get service resto -ojsonpath='{..nodePort}')
+```
+
+Now browse http://IP:PORTRESTO
+
+# install prometheus (and grafana and node exporter)
+
+We are going to use prometheus-operator. Commands are adapted from $GOPATH/src/github.com/dbenque/prometheus-instrumentation-demo/vendor/github.com/coreos/prometheus-operator/contrib/kube-prometheus/hack/cluster-monitoring/deploy
+
+```
+NAMESPACE=monitoring
+kk create namespace $NAMESPACE
+kk config set-context $(kubectl config current-context) --namespace=$NAMESPACE
+
+KUBEPROM=$GOPATH/src/github.com/dbenque/prometheus-instrumentation-demo/vendor/github.com/coreos/prometheus-operator/contrib/kube-prometheus
+
+#kk apply -f ../prometheus/prom-kube-bundle.yaml
+kk apply -f $KUBEPROM/manifests/prometheus-operator
+
+# Wait for CRDs to be ready.
+until kk get customresourcedefinitions servicemonitors.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
+until kk get customresourcedefinitions prometheuses.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
+until kk get customresourcedefinitions alertmanagers.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
+until kk get servicemonitors.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
+until kk get prometheuses.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
+until kk get alertmanagers.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
+
+kk apply -f $KUBEPROM/manifests/node-exporter
+kk apply -f $KUBEPROM/manifests/kube-state-metrics
+kk apply -f $KUBEPROM/manifests/grafana/grafana-credentials.yaml
+kk apply -f $KUBEPROM/manifests/grafana
+#find $KUBEPROM/manifests/prometheus -type f ! -name prometheus-k8s-roles.yaml ! -name prometheus-k8s-role-bindings.yaml -exec kk --namespace "$NAMESPACE" apply -f {} \;
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-roles.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-role-bindings.yaml
+kk apply -f $KUBEPROM/manifests/alertmanager/
+
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-kube-scheduler.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-kube-state-metrics.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-rules.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-node-exporter.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-account.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-prometheus-operator.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-alertmanager.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-prometheus.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-apiserver.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-kube-controller-manager.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s.yaml
+kk apply -f $KUBEPROM/manifests/prometheus/prometheus-k8s-service-monitor-kubelet.yaml
+
+```
---
+++ vendor/github.com/coreos/prometheus-operator/cmd/operator/main.go
@@ -0,0 +1,155 @@
+// Copyright 2016 The prometheus-operator Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/sync/errgroup"
+	kapi "k8s.io/client-go/pkg/api"
+
+	"github.com/coreos/prometheus-operator/pkg/alertmanager"
+	"github.com/coreos/prometheus-operator/pkg/api"
+	monitoringv1 "github.com/coreos/prometheus-operator/pkg/client/monitoring/v1"
+	"github.com/coreos/prometheus-operator/pkg/k8sutil"
+	"github.com/coreos/prometheus-operator/pkg/migrator"
+	prometheuscontroller "github.com/coreos/prometheus-operator/pkg/prometheus"
+	"github.com/go-kit/kit/log"
+)
+
+var (
+	cfg prometheuscontroller.Config
+)
+
+func init() {
+	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	flagset.StringVar(&cfg.Host, "apiserver", "", "API Server addr, e.g. ' - NOT RECOMMENDED FOR PRODUCTION - http://127.0.0.1:8080'. Omit parameter to run in on-cluster mode and utilize the service account token.")
+	flagset.StringVar(&cfg.TLSConfig.CertFile, "cert-file", "", " - NOT RECOMMENDED FOR PRODUCTION - Path to public TLS certificate file.")
+	flagset.StringVar(&cfg.TLSConfig.KeyFile, "key-file", "", "- NOT RECOMMENDED FOR PRODUCTION - Path to private TLS certificate file.")
+	flagset.StringVar(&cfg.TLSConfig.CAFile, "ca-file", "", "- NOT RECOMMENDED FOR PRODUCTION - Path to TLS CA file.")
+	flagset.StringVar(&cfg.KubeletObject, "kubelet-service", "", "Service/Endpoints object to write kubelets into in format \"namespace/name\"")
+	flagset.BoolVar(&cfg.TLSInsecure, "tls-insecure", false, "- NOT RECOMMENDED FOR PRODUCTION - Don't verify API server's CA certificate.")
+	flagset.StringVar(&cfg.PrometheusConfigReloader, "prometheus-config-reloader", "quay.io/coreos/prometheus-config-reloader:v0.0.2", "Config and rule reload image")
+	flagset.StringVar(&cfg.ConfigReloaderImage, "config-reloader-image", "quay.io/coreos/configmap-reload:v0.0.1", "Reload Image")
+	flagset.StringVar(&cfg.AlertmanagerDefaultBaseImage, "alertmanager-default-base-image", "quay.io/prometheus/alertmanager", "Alertmanager default base image")
+	flagset.StringVar(&cfg.PrometheusDefaultBaseImage, "prometheus-default-base-image", "quay.io/prometheus/prometheus", "Prometheus default base image")
+	flagset.StringVar(&cfg.Namespace, "namespace", kapi.NamespaceAll, "Namespace to scope the interaction of the Prometheus Operator and the apiserver.")
+	flagset.Var(&cfg.Labels, "labels", "Labels to be add to all resources created by the operator")
+	flagset.StringVar(&cfg.CrdGroup, "crd-apigroup", monitoringv1.Group, "prometheus CRD  API group name")
+	flagset.Parse(os.Args[1:])
+
+}
+
+func Main() int {
+	logger := log.NewContext(log.NewLogfmtLogger(os.Stdout)).
+		With("ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+
+	r := prometheus.NewRegistry()
+	r.MustRegister(prometheus.NewGoCollector())
+
+	po, err := prometheuscontroller.New(cfg, logger.With("component", "prometheusoperator"))
+	if err != nil {
+		fmt.Fprint(os.Stderr, "instantiating prometheus controller failed: ", err)
+		return 1
+	}
+
+	ao, err := alertmanager.New(cfg, logger.With("component", "alertmanageroperator"))
+	if err != nil {
+		fmt.Fprint(os.Stderr, "instantiating alertmanager controller failed: ", err)
+		return 1
+	}
+
+	mux := http.NewServeMux()
+	web, err := api.New(cfg, logger.With("component", "api"))
+	if err != nil {
+		fmt.Fprint(os.Stderr, "instantiating api failed: ", err)
+		return 1
+	}
+
+	web.Register(mux)
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Fprint(os.Stderr, "listening port 8080 failed", err)
+		return 1
+	}
+
+	po.RegisterMetrics(r)
+	ao.RegisterMetrics(r)
+	mux.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
+	mux.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
+	mux.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
+	mux.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
+	mux.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
+	mux.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
+
+	conf, err := k8sutil.NewClusterConfig(cfg.Host, cfg.TLSInsecure, &cfg.TLSConfig)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "failed to instantiate cluster config: ", err)
+		return 1
+	}
+
+	m, err := migrator.NewMigrator(conf, logger)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "failed to instantiate migrator: ", err)
+		return 1
+	}
+
+	err = m.RunMigration()
+	if err != nil {
+		fmt.Fprint(os.Stderr, "failed to run migrations: ", err)
+		return 1
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	wg, ctx := errgroup.WithContext(ctx)
+
+	wg.Go(func() error { return po.Run(ctx.Done()) })
+	wg.Go(func() error { return ao.Run(ctx.Done()) })
+
+	srv := &http.Server{Handler: mux}
+	go srv.Serve(l)
+
+	term := make(chan os.Signal)
+	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case <-term:
+		logger.Log("msg", "Received SIGTERM, exiting gracefully...")
+	case <-ctx.Done():
+	}
+
+	cancel()
+	if err := wg.Wait(); err != nil {
+		logger.Log("msg", "Unhandled error received. Exiting...", "err", err)
+		return 1
+	}
+
+	return 0
+}
+
+func main() {
+	os.Exit(Main())
+}
