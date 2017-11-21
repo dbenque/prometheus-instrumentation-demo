fa141114f566cff1901d944b6f9ff4e41201ed5c
David BENQUE <dbenque@amadeus.com>
2017-11-21 10:28:17 +0100

    order start

--- prometheus/launch.local.sh
+++ prometheus/launch.local.sh
@@ -9,10 +9,10 @@ sleep 2
 
 chmod +755 local.prometheus.yml
 
-echo -e "\e[1;33mLaunching prometheus local\e[0m"
-docker run -d --name=prom-local --net=host -v $PWD/local.prometheus.yml:/etc/prometheus/prometheus.yml --rm prom/prometheus
 echo -e "\e[1;33mLaunching prometheus node exporter\e[0m"
 docker run -d --name=prom-nodeexporter --net="host" --pid="host" --rm prom/node-exporter
+echo -e "\e[1;33mLaunching prometheus local\e[0m"
+docker run -d --name=prom-local --net=host -v $PWD/local.prometheus.yml:/etc/prometheus/prometheus.yml --rm prom/prometheus
 echo -e "\e[1;33mLaunching grafana\e[0m"
 #docker run -d --name=grafana --net=host -e "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource" --rm grafana/grafana
 docker run -d --name=grafana --net=host --rm grafana/grafana
